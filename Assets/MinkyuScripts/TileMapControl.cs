f062b004c892ffc9f16348b3de4028b40ac6ff0c
scotcy <kimjh470@naver.com>
2024-01-15 16:45:57 +0900

    [💫 Feature] TilemapManager.cs, PlayerControlWall.cs 추가
    해당 cs의 기능을 구현

--- Assets/MinkyuScripts/TileMapControl.cs
+++ Assets/MinkyuScripts/TileMapControl.cs
@@ -5,20 +5,17 @@ using UnityEngine.Tilemaps;
 
 public class TileMapControl : MonoBehaviour
 {
-    public Tilemap tilemap;
     public TileBase newTile;
 
     public void CreateTile(int x, int y)
     {
         Vector3 cellPosition = new Vector3(x, y, 0);
-        tilemap.SetTile(tilemap.WorldToCell(cellPosition), newTile);
-        Debug.Log(cellPosition);
-        // GetTile 을 사용하여 있는지 확인
+        TilemapManager.instance.tilemap.SetTile(TilemapManager.instance.tilemap.WorldToCell(cellPosition), newTile);
     }
 
     public void DestroyTile(int x, int y)
     {
         Vector3 cellPosition = new Vector3(x, y, 0);
-        tilemap.SetTile(tilemap.WorldToCell(cellPosition), null);
+        TilemapManager.instance.tilemap.SetTile(TilemapManager.instance.tilemap.WorldToCell(cellPosition), null);
     }
 }
---
+++ Assets/MinkyuScripts/TilemapManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+[System.Serializable]
+public class TileInfo
+{
+    public TileBase tile;
+    public float HP;
+
+    // 다른 속성들을 필요에 따라 추가
+}
+public class TilemapManager : MonoBehaviour
+{
+    public static TilemapManager instance;
+
+    public Tilemap tilemap;
+    public Dictionary<Vector3Int, TileInfo> wallDictionary = new Dictionary<Vector3Int, TileInfo>();
+    public Tilemap groundBound;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        // Bounds는 구조체
+        // 해당 Tile의 크기만큼 싹 for문을 돌린다. tile이 있으면 Dictionary에 추가. 각각 TileInfo의 값을 받는다.
+        BoundsInt bounds = tilemap.cellBounds;
+        for (int x = bounds.x; x < bounds.x + bounds.size.x; x++)
+        {
+            for (int y = bounds.y; y < bounds.y + bounds.size.y; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                TileBase tile = tilemap.GetTile(cellPosition);
+
+                if (tile != null)
+                {
+                    wallDictionary[cellPosition] = new TileInfo
+                    {
+                        tile = tile,
+                        HP = 100f
+                    };
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+        Gizmos.color = Color.green;
+        for (int x = bounds.x; x < bounds.x + bounds.size.x; x++)
+        {
+            for (int y = bounds.y; y < bounds.y + bounds.size.y; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                Vector3 cellCenter = tilemap.GetCellCenterWorld(cellPosition);
+                Gizmos.DrawWireCube(cellCenter, tilemap.cellSize);
+            }
+        }
+    }
+}
