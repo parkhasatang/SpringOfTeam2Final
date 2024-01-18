fe5e070bc242d79c3ee209275f34309a6e7ba89f
Leejungsuk96 <svwsv3462@gmail.com>
2024-01-18 13:15:46 +0900

    [Temporary] MiningAttack Function

--- Assets/JungSuk/Scripts/Player/PlayerBehavior/Attack.cs
+++ Assets/JungSuk/Scripts/Player/PlayerBehavior/Attack.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     private CharacterController controller;
-
+    private CharacterStatHandler statsHandler;
+    [SerializeField] private Camera mainCamera;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        statsHandler = GetComponent<CharacterStatHandler>();
         
     }
 
@@ -18,10 +20,33 @@ public class Attack : MonoBehaviour
     private void Start()
     {
         controller.OnAttackEvent += PlayerAttack;
+        
     }
 
     private void PlayerAttack()
     {
-        Debug.Log("°ø°ÝÀÌ´å");
+               
     }
 }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+        
+    //    Vector3Int mousePos = new Vector3Int(Mathf.FloorToInt(collision.transform.position.x), Mathf.FloorToInt(collision.transform.position.y), 0);
+    //    TilemapManager.instance.wallDictionary.TryGetValue(mousePos, out TileInfo tileInfo);
+ 
+    //    if(tileInfo.HP > 0)
+    //    {
+    //        tileInfo.HP -= statsHandler.CurrentStats.baseStatsSO.miningAttack;
+
+    //        if(tileInfo.HP <= 0)
+    //        {
+    //            TilemapManager.instance.tilemap.SetTile(TilemapManager.instance.tilemap.WorldToCell(mousePos), null);
+    //        }
+
+    //    }            
+        
+    //}
+
+
+
