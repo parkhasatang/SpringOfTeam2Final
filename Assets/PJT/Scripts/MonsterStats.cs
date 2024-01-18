d53be26e8ded020ffd092b51622dbbe9242de671
[Tanha] <[julus8238@gmail.com]>
2024-01-18 09:10:31 +0900

    feat.MonsterStat
    몬스터 스탯

---
+++ Assets/PJT/Scripts/MonsterStats.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class MonsterStats
+{
+    public float currentHP;
+    public float followDistance;
+    public float attackRange;
+
+    public BaseStatsSO monsterBaseStatsSO;
+
+}
