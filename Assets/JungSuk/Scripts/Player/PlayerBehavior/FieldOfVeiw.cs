6f8ff7c70ab2d6ff9e2b0fb50f85ed1ac3bc5e70
Leejungsuk96 <svwsv3462@gmail.com>
2024-01-30 00:05:29 +0900

    [Feature] Player FOV

---
+++ Assets/JungSuk/Scripts/Player/PlayerBehavior/FieldOfVeiw.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class FieldOfView : MonoBehaviour
+{
+    [Range(0, 360)] public float fov = 90f;
+    public int rayCount = 90;
+    public float viewDistance = 10f;
+    public LayerMask layerMask;                     
+    private Mesh mesh;                          
+
+    private void Start()
+    {
+      
+        mesh = new Mesh();
+        mesh.name = "FOV";
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private void LateUpdate()
+    {
+        //Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        //transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(new Vector3(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y, 0)));
+        DrawFOV();
+    }
+
+    private void DrawFOV()
+    {
+        float angle = fov * 0.5f;
+        float angleIncrease = fov / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = Vector3.zero;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, GetVectorFromAngle(transform.eulerAngles.z + angle), viewDistance, layerMask);
+            if (raycastHit2D.collider == null)
+            {
+                vertex = GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                vertex = GetVectorFromAngle(angle) * (raycastHit2D.distance / viewDistance) * viewDistance;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (0 < i)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            ++vertexIndex;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+    private int GetAngleFromVector(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        int angle = Mathf.RoundToInt(n);
+
+        return angle;
+    }
+}
+
