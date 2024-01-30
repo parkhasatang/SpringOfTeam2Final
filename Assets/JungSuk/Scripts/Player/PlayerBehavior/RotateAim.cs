bee9888d1cd4bcfcd9af3cb71794a7092972fa30
Leejungsuk96 <svwsv3462@gmail.com>
2024-01-30 22:02:24 +0900

    [Fix] 플레이어 애니메이션 수정

--- Assets/JungSuk/Scripts/Player/PlayerBehavior/RotateAim.cs
+++ Assets/JungSuk/Scripts/Player/PlayerBehavior/RotateAim.cs
@@ -32,25 +32,9 @@ public class RotateAim : MonoBehaviour
     {
         float angle = Mathf.Atan2(value.y, value.x)*Mathf.Rad2Deg;
         weaponPivot.rotation = Quaternion.Euler(playertransfrom.position.x, playertransfrom.position.y, angle);
-        if( -45f< angle && angle < 45f)
-        {
-            playerAnimator.SetInteger("AimLotation", 3);            
-        }
-
-        if(45f<= angle && angle < 135f)
-        {           
-            playerAnimator.SetInteger("AimLotation", 1);
-        }
-
-        if(135f <= angle && angle <= 180f || -180f < angle && angle < -135f)
-        {          
-            playerAnimator.SetInteger("AimLotation", 2);
-        }
-
-        if(-135f < angle && angle <= -45f)
-        {            
-            playerAnimator.SetInteger("AimLotation", 0);
-        }
+        
+        playerAnimator.SetFloat("X",value.x);
+        playerAnimator.SetFloat ("Y",value.y);
     }
 
   
