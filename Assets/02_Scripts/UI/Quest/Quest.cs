d7fd019a47406ffea47bd13e30adfe395b0a87e9
scotcy <kimjh470@naver.com>
2024-02-20 21:10:41 +0900

    [Temp]

---
+++ Assets/02_Scripts/UI/Quest/Quest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest : MonoBehaviour
+{
+    public Image stuffImage;
+    private Item requestItem;
+    private Item rewardItem;
+    private int requestCount;
+    private int rewardCount;
+    public TMP_Text countTxt;
+
+    // 랜덤 퀘스트 주기.
+    public void RandomQuest()
+    {
+
+    }
+
+    // 퀘스트 조건을 충족했는지 확인
+    public void CheckRequst()
+    {
+
+    }
+}
