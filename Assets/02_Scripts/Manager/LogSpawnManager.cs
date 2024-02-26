6fa855081ca57b6308173bafa3e53fcf8e1319a5
Leejungsuk96 <svwsv3462@gmail.com>
2024-02-26 17:03:37 +0900

    [Fixed]

--- Assets/02_Scripts/Manager/LogSpawnManager.cs
+++ Assets/02_Scripts/Manager/LogSpawnManager.cs
@@ -31,7 +31,7 @@ public class LogSpawnManager : MonoBehaviour
         {
             SpawnLogTrap();
 
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(3f);
         }
     }
    
