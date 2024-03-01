6c0ca903fe54aa092c4a7fea0368f679a160ed15
Leejungsuk96 <svwsv3462@gmail.com>
2024-03-02 01:30:49 +0900

    [FIxed] DataManager

--- Assets/02_Scripts/Manager/DataManager.cs
+++ Assets/02_Scripts/Manager/DataManager.cs
@@ -20,17 +20,36 @@ public class GameData
 {
     // 이름, 레벨, 코인, 착용중인 무기    
     public Vector3 playerPosition;
-    public Item[] slotItemData;
-
+    public SlotNum[] _slotNum;
+    public List<SlotData> _slots;   
+    
+    public GameData(int num)
+    {
+        _slotNum = new SlotNum[num];
+        _slots = new List<SlotData>();
+        for (int i = 0; i < num; i++)
+        {
+            _slotNum[i] = new SlotNum();
+            SlotData slot = new SlotData()
+            {
+                isEmpty = true,
+                isChoose = false,
+                item = null,
+                stack = 0
+            };
+            _slots.Add(slot);
+        }
+    }
 }
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
     public GameObject player;
+    private Inventory inventory;
     string path;
     string fileName = "SaveFile";
         
-    GameData newGameData = new GameData();
+    GameData newGameData = new GameData(26);
 
     
     private void Awake()
@@ -38,14 +57,20 @@ public class DataManager : MonoBehaviour
         instance = this;
         DontDestroyOnLoad(this.gameObject);
         path = Application.persistentDataPath + "/";
-        LoadData();
+        inventory = player.GetComponent<Inventory>();                
     }
     // Start is called before the first frame update
     void Start()
     {
+        LoadData();
         player.transform.position = newGameData.playerPosition;
-    }
 
+        for (int i = 0; i < inventory.invenSlot.Length; i++)
+        {
+            inventory.invenSlot[i] = newGameData._slotNum[i];
+            inventory.slots[i] = newGameData._slots[i];
+        }
+    }   
     // Update is called once per frame
     void Update()
     {
@@ -55,6 +80,12 @@ public class DataManager : MonoBehaviour
     public void SaveData()
     {
         newGameData.playerPosition = player.transform.position;
+        newGameData._slotNum = new SlotNum[inventory.invenSlot.Length];
+        for (int i = 0; i < inventory.invenSlot.Length; i++)
+        {
+            newGameData._slotNum[i] = inventory.invenSlot[i];
+            newGameData._slots[i] = inventory.slots[i];
+        }
         string data = JsonUtility.ToJson(newGameData);
         File.WriteAllText(path + fileName, data);
     }
